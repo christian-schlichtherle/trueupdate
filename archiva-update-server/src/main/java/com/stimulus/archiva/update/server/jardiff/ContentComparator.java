505a2430bea5dc7f690ffbdb03688e60f8e7106d
Christian Schlichtherle <christian@schlichtherle.de>
2013-07-25 10:55:45 +0200

    Added Digests.

--- archiva-update-server/src/main/java/com/stimulus/archiva/update/server/jardiff/ContentComparator.java
+++ archiva-update-server/src/main/java/com/stimulus/archiva/update/server/jardiff/ContentComparator.java
@@ -4,13 +4,12 @@
  */
 package com.stimulus.archiva.update.server.jardiff;
 
-import com.stimulus.archiva.update.core.io.Source;
-
 import javax.annotation.concurrent.Immutable;
-import java.io.*;
-import java.security.*;
+import java.io.IOException;
+import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.jar.JarEntry;
+import static com.stimulus.archiva.update.server.jardiff.Digests.*;
 
 /**
  * Compares the contents of two JAR entries in two different JAR files.
@@ -45,33 +44,5 @@ public class ContentComparator implements Comparator {
         // cryptographically strong message digest.
         final MessageDigest digest = sha1();
         return Arrays.equals(digest(digest, entryInFile1), digest(digest, entryInFile2));
-    }
-
-    private static MessageDigest sha1() { return digest("SHA-1"); }
-
-    private static MessageDigest digest(final String algorithm) {
-        try {
-            return MessageDigest.getInstance(algorithm);
-        } catch (NoSuchAlgorithmException ex) {
-            throw new AssertionError(
-                    "JRE doesn't implement standard message digest SHA-1. See http://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#MessageDigest .",
-                    ex);
-        }
-    }
-
-    private static byte[] digest(
-            final MessageDigest digest,
-            final Source source)
-    throws IOException {
-        digest.reset();
-        final byte[] buffer = new byte[8 * 1024];
-        try (InputStream in = source.input()) {
-            int read;
-            do {
-                read = in.read(buffer);
-                if (0 < read) digest.update(buffer, 0, read);
-            } while (0 <= read);
-        }
-        return digest.digest();
     }
 }
---
+++ archiva-update-server/src/test/scala/com/stimulus/archiva/update/server/jardiff/DigestsIT.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2005-2013 Stimulus Software.
+ * All rights reserved. Use is subject to license terms.
+ */
+package com.stimulus.archiva.update.server.jardiff
+
+import org.scalatest.WordSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers._
+import org.scalatest.prop.PropertyChecks._
+import com.stimulus.archiva.update.core.io.Sources
+
+/**
+ * @author Christian Schlichtherle
+ */
+@RunWith(classOf[JUnitRunner])
+class DigestsIT extends WordSpec {
+
+  "Computation of digests" should {
+    "work as expected" in {
+      val table = Table(
+        ("SHA-1", "resource name"),
+        ("4690d26560ecaed2e9820e2135cd8eae76003f9f", "test1.jar"),
+        ("2ec083ed5a4a914af0d8faf84a9f03c9d4dc2686", "test2.jar"),
+        // Note that the most significant bit is set to test signum conversion
+        ("f3172822c7d08f23764aa5baee9d73ef32797b46", "twoTimesHelloWorld")
+      )
+      forAll(table) { (sha1, resourceName) =>
+        Digests.hexString(Digests.digest(Digests.sha1,
+          Sources.forResource(resourceName, classOf[DigestsIT]))) should equal (sha1)
+      }
+    }
+  }
+}
