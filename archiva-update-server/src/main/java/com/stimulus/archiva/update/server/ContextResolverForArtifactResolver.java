bdcda8340b5ced87327865c476bacaab63b49430
Christian Schlichtherle <christian@schlichtherle.de>
2013-08-05 12:00:55 +0200

    Simplified configuration.

--- archiva-update-server/src/main/java/com/stimulus/archiva/update/server/ContextResolverForArtifactResolver.java
+++ archiva-update-server/src/main/java/com/stimulus/archiva/update/server/ContextResolverForArtifactResolver.java
@@ -5,24 +5,20 @@
 package com.stimulus.archiva.update.server;
 
 import com.stimulus.archiva.update.core.artifact.ArtifactResolver;
+import com.stimulus.archiva.update.maven.*;
 import javax.annotation.concurrent.ThreadSafe;
 import javax.ws.rs.ext.*;
 
 /**
  * A context resolver which resolves {@link ArtifactResolver}s to a singleton
- * {@link ConfiguredMavenArtifactResolver}.
+ * {@link MavenArtifactResolver}.
  */
 @ThreadSafe
 @Provider
 public class ContextResolverForArtifactResolver
 implements ContextResolver<ArtifactResolver> {
 
-    private volatile ArtifactResolver artifactResolver;
-
     @Override public ArtifactResolver getContext(Class<?> type) {
-        final ArtifactResolver artifactResolver = this.artifactResolver;
-        return null != artifactResolver
-                ? artifactResolver
-                : (this.artifactResolver = new ConfiguredMavenArtifactResolver());
+        return MavenArtifactResolver.getInstance();
     }
 }
