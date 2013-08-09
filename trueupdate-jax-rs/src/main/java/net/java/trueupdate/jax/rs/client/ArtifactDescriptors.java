d1615da636136b194292a7a0549ab01e951c479f
Christian Schlichtherle <christian@schlichtherle.de>
2013-08-09 23:27:30 +0200

    Optimized.

--- trueupdate-jax-rs/src/main/java/net/java/trueupdate/jax/rs/client/ArtifactDescriptors.java
+++ trueupdate-jax-rs/src/main/java/net/java/trueupdate/jax/rs/client/ArtifactDescriptors.java
@@ -5,6 +5,7 @@
 package net.java.trueupdate.jax.rs.client;
 
 import com.sun.jersey.core.util.MultivaluedMapImpl;
+import java.util.concurrent.Callable;
 import javax.ws.rs.core.MultivaluedMap;
 import net.java.trueupdate.artifact.spec.ArtifactDescriptor;
 
@@ -18,20 +19,34 @@ final class ArtifactDescriptors {
     private ArtifactDescriptors() { }
 
     /**
-     * Returns a multi valued map with query parameters for the given artifact
-     * descriptor.
+     * Returns a new multi valued map with query parameters for the given
+     * artifact descriptor.
      *
      * @param descriptor the artifact descriptor.
-     * @return the multi valued map with the query parameters.
+     * @return the new multi valued map with the query parameters.
      */
     static MultivaluedMap<String, String> queryParameters(
             final ArtifactDescriptor descriptor) {
-        final MultivaluedMap<String, String> map = new MultivaluedMapImpl();
-        map.putSingle("groupId", descriptor.groupId());
-        map.putSingle("artifactId", descriptor.artifactId());
-        map.putSingle("version", descriptor.version());
-        map.putSingle("classifier", descriptor.classifier());
-        map.putSingle("extension", descriptor.extension());
-        return map;
+
+        class QueryParameters implements Callable<MultivaluedMap<String, String>> {
+
+            final MultivaluedMap<String, String> map = new MultivaluedMapImpl();
+
+            @SuppressWarnings("ReturnOfCollectionOrArrayField")
+            @Override public MultivaluedMap<String, String> call() {
+                put("groupId", descriptor.groupId());
+                put("artifactId", descriptor.artifactId());
+                put("version", descriptor.version());
+                put("classifier", descriptor.classifier());
+                put("extension", descriptor.extension());
+                return map;
+            }
+
+            void put(String key, String value) {
+                if (!value.isEmpty()) map.putSingle(key, value);
+            }
+        } // QueryParameters
+
+        return new QueryParameters().call();
     }
 }
