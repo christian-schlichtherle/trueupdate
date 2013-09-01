7dfbf1df5225d30d0d836d146e8013fa83885b94
Christian Schlichtherle <christian@schlichtherle.de>
2013-09-01 11:37:20 +0200

    Simplified.

--- trueupdate-server/trueupdate-server-impl-maven/src/main/java/net/java/trueupdate/server/impl/maven/MavenUpdateServer.java
+++ trueupdate-server/trueupdate-server-impl-maven/src/main/java/net/java/trueupdate/server/impl/maven/MavenUpdateServer.java
@@ -4,49 +4,90 @@
  */
 package net.java.trueupdate.server.impl.maven;
 
+import java.lang.reflect.UndeclaredThrowableException;
+import java.net.URI;
 import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+import javax.servlet.ServletContext;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.ext.ContextResolver;
+import net.java.trueupdate.artifact.impl.maven.MavenArtifactResolver;
 import net.java.trueupdate.artifact.spec.ArtifactResolver;
+import net.java.trueupdate.core.io.Source;
+import net.java.trueupdate.core.io.Sources;
 import net.java.trueupdate.jax.rs.server.BasicUpdateServer;
-import net.java.trueupdate.shed.Objects;
+import net.java.trueupdate.shed.SystemProperties;
 
 /**
  * A an artifact update server which uses a maven artifact resolver.
+ * For best performance, this should be used like a singleton
+ * - see {@link MavenUpdateServerApplication}.
+ * Once {@linkplain #setServletContext initialized}, it is safe to use this
+ * class in multiple threads.
  *
  * @author Christian Schlichtherle
  */
 @Path("/")
+@ThreadSafe
 public final class MavenUpdateServer extends BasicUpdateServer {
 
-    private @Nullable ArtifactResolver artifactResolver;
+    private volatile @Nullable URI configuration;
 
-    /**
-     * Constructs a maven update server with the given configuration URI.
-     */
-    public MavenUpdateServer(final ArtifactResolver artifactResolver) {
-        this.artifactResolver = Objects.requireNonNull(artifactResolver);
-    }
+    private volatile @Nullable ArtifactResolver artifactResolver;
 
     /**
      * Constructs a maven update server.
-     * Use of this constructor requires calling {@link #setContextResolver}
+     * Use of this constructor requires calling {@link #setServletContext}
      * before use.
      */
     public MavenUpdateServer() { }
 
     /**
      * Calling this method is required when using the no-arg constructor.
-     * This method looks up the artifact resolver from the context resolver
-     * with a {@code null} type parameter.
+     * This method looks up the configuration URI from the servlet context
+     * init-parameter with the name "configuration".
+     * As a side effect, the {@linkplain #artifactResolver() artifact resolver}
+     * gets reloaded upon the next call.
+     *
+     * @throws NullPointerException if {@code context} is {@code null} or has
+     *         no setServletContext-parameter named "configuration".
+     * @throws IllegalArgumentException if the parameter value is not a valid
+     *         URI.
      */
     @Context
-    public void setContextResolver(
-            ContextResolver<ArtifactResolver> contextResolver) {
-        this.artifactResolver = contextResolver.getContext(null);
+    public void setServletContext(final ServletContext context) {
+        this.configuration = URI.create(SystemProperties.resolve(
+                context.getInitParameter("configuration")));
+        artifactResolver = null;
     }
 
-    @Override
-    protected ArtifactResolver artifactResolver() { return artifactResolver; }
+    @Override protected ArtifactResolver artifactResolver() {
+        final ArtifactResolver ar = this.artifactResolver;
+        return null != ar
+                ? ar
+                : (this.artifactResolver = loadArtifactResolver());
+    }
+
+    private ArtifactResolver loadArtifactResolver() {
+        try { return MavenArtifactResolver.decodeFromXml(source()); }
+        catch (RuntimeException ex) { throw ex; }
+        catch (Exception ex) { throw new UndeclaredThrowableException(ex); }
+    }
+
+    private Source source() throws Exception {
+        final URI uri = configuration;
+        return uri.isAbsolute()
+                ? Sources.forUrl(uri.toURL())
+                : Sources.forResource(removeLeadingSlashes(uri.getPath()),
+                                      contextClassLoader());
+    }
+
+    private static String removeLeadingSlashes(String string) {
+        while (string.startsWith("/")) string = string.substring(1);
+        return string;
+    }
+
+    private static ClassLoader contextClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
 }
--- trueupdate-server/trueupdate-server-impl-maven/src/main/java/net/java/trueupdate/server/impl/maven/MavenUpdateServerApplication.java
+++ trueupdate-server/trueupdate-server-impl-maven/src/main/java/net/java/trueupdate/server/impl/maven/MavenUpdateServerApplication.java
@@ -5,21 +5,54 @@
 package net.java.trueupdate.server.impl.maven;
 
 import java.util.*;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import net.java.trueupdate.jax.rs.server.UpdateServiceExceptionMapper;
+import net.java.trueupdate.shed.Objects;
 
 /**
+ * An application which provides the class {@link UpdateServiceExceptionMapper}
+ * and a new {@link MavenUpdateServer}.
+ * Once {@linkplain #setServletContext initialized}, it is safe to use this
+ * class in multiple threads.
+ *
  * @author Christian Schlichtherle
  */
 @ApplicationPath("/")
+@ThreadSafe
 public final class MavenUpdateServerApplication extends Application {
 
+    private volatile @Nullable ServletContext context;
+
+    /**
+     * Constructs a maven update server application.
+     * Use of this constructor requires calling {@link #setServletContext}
+     * before use.
+     */
+    public MavenUpdateServerApplication() { }
+
+    /**
+     * Calling this method is required when using the no-arg constructor.
+     */
+    @Context
+    public void setServletContext(final ServletContext context) {
+        this.context = Objects.requireNonNull(context);
+    }
+
+    /** Returns a set with {@link UpdateServiceExceptionMapper}. */
     @SuppressWarnings("unchecked")
     @Override public Set<Class<?>> getClasses() {
-        return new HashSet<Class<?>>(Arrays.asList(
-                UpdateServiceExceptionMapper.class,
-                MavenArtifactResolverContextResolver.class,
-                MavenUpdateServer.class));
+        return (Set) Collections.singleton(UpdateServiceExceptionMapper.class);
+    }
+
+    /** Returns a set with a new {@link MavenUpdateServer}. */
+    @Override public Set<Object> getSingletons() {
+        final MavenUpdateServer mus = new MavenUpdateServer();
+        mus.setServletContext(context);
+        return Collections.singleton((Object) mus);
     }
 }
