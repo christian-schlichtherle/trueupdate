e5a54cf34cce6639c022c4fcdaca4817beac18af
Christian Schlichtherle <christian@schlichtherle.de>
2013-09-04 18:59:47 +0200

    Javadoc and FindBugs.

---
+++ trueupdate-core/src/main/java/net/java/trueupdate/core/zip/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2013 Stimulus Software & Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+/**
+ * Specifies ZIP I/O services.
+ *
+ * @author Christian Schlichtherle
+ */
+@javax.annotation.ParametersAreNonnullByDefault @javax.annotation.Nonnull
+package net.java.trueupdate.core.zip;
