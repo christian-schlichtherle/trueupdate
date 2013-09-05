8c06b379b0ccc4ca493dac76bf57fcd5642a2ceb
Christian Schlichtherle <christian@schlichtherle.de>
2013-09-05 16:34:32 +0200

    Added Zip class for testing.

---
+++ trueupdate-manager/trueupdate-manager-core/src/test/java/net/java/trueupdate/manager/core/io/Zip.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013 Stimulus Software & Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.trueupdate.manager.core.io;
+
+import java.io.File;
+import java.util.Locale;
+
+/**
+ * @author Christian Schlichtherle
+ */
+public class Zip {
+
+    public static void main(final String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: Zip <zipFile> <fileOrDirectory>");
+            System.exit(1);
+        }
+        final File zipFile = new File(args[0]);
+        final File fileOrDirectory = new File(args[1]);
+        final long started = System.currentTimeMillis();
+        Files.zip(zipFile, fileOrDirectory);
+        final long duration = System.currentTimeMillis() - started;
+        System.out.printf(Locale.ENGLISH, "Written ZIP file in %.3f seconds.\n", duration / 1000.0);
+    }
+}
