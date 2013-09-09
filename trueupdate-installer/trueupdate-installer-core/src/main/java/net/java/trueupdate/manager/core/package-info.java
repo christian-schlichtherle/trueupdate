9d2a79b31f12501c5fef11578e06e6f4724fab81
Christian Schlichtherle <christian@schlichtherle.de>
2013-09-09 13:31:01 +0200

    Introduced TrueUpdate Installer Core.

---
+++ trueupdate-installer/trueupdate-installer-core/src/main/java/net/java/trueupdate/manager/core/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2013 Stimulus Software & Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+/**
+ * Provides a basic implementation of a manager for updating applications.
+ *
+ * @author Christian Schlichtherle
+ */
+@javax.annotation.ParametersAreNonnullByDefault @javax.annotation.Nonnull
+package net.java.trueupdate.manager.core;
