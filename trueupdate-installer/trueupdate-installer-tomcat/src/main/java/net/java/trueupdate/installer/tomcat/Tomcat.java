233f28d3f6ae44a477f34585a903ae0e47f265f7
Christian Schlichtherle <christian@schlichtherle.de>
2013-09-09 10:45:26 +0200

    Simplified.

--- trueupdate-installer/trueupdate-installer-tomcat/src/main/java/net/java/trueupdate/installer/tomcat/Tomcat.java
+++ trueupdate-installer/trueupdate-installer-tomcat/src/main/java/net/java/trueupdate/installer/tomcat/Tomcat.java
@@ -4,16 +4,8 @@
  */
 package net.java.trueupdate.installer.tomcat;
 
-import javax.management.AttributeNotFoundException;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanServer;
-import javax.management.MBeanServerFactory;
-import javax.management.ObjectName;
-import org.apache.catalina.Engine;
-import org.apache.catalina.Host;
-import org.apache.catalina.LifecycleListener;
-import org.apache.catalina.Server;
-import org.apache.catalina.Service;
+import javax.management.*;
+import org.apache.catalina.*;
 import org.apache.catalina.startup.HostConfig;
 
 /**
@@ -24,18 +16,15 @@ import org.apache.catalina.startup.HostConfig;
 final class Tomcat {
 
     static HostConfig resolveHostConfig() throws Exception {
-        final ObjectName pattern = new ObjectName("*:type=Server");
+        final ObjectName pattern = new ObjectName("*:type=Engine");
         for (final MBeanServer mbs : MBeanServerFactory.findMBeanServer(null)) {
             for (final ObjectName on : mbs.queryNames(pattern, null)) {
                 try {
-                    final Server server = (Server) mbs.getAttribute(on, "managedResource");
-                    for (final Service service : server.findServices()) {
-                        final Engine engine = (Engine) service.getContainer();
-                        final Host host = (Host) engine.findChild(engine.getDefaultHost());
-                        for (final LifecycleListener listener : host.findLifecycleListeners())
-                            if (listener instanceof HostConfig)
-                                return (HostConfig) listener;
-                    }
+                    final Engine engine = (Engine) mbs.getAttribute(on, "managedResource");
+                    final Host host = (Host) engine.findChild(engine.getDefaultHost());
+                    for (final LifecycleListener listener : host.findLifecycleListeners())
+                        if (listener instanceof HostConfig)
+                            return (HostConfig) listener;
                 } catch (InstanceNotFoundException ex) {
                 } catch (AttributeNotFoundException ex) {
                 }
