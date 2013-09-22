b25e91b9bf41a06442f6e4773ffb2f93819b141a
Christian Schlichtherle <christian@schlichtherle.de>
2013-09-22 11:51:18 +0200

    Saved some heap space.

--- trueupdate-jms/src/main/java/net/java/trueupdate/jms/JAXB.java
+++ trueupdate-jms/src/main/java/net/java/trueupdate/jms/JAXB.java
@@ -25,19 +25,20 @@ final class JAXB {
         }
     }
 
-    private static final UpdateMessageAdapter ADAPTER =
-            new UpdateMessageAdapter();
-
     static String encode(final UpdateMessage message) throws Exception {
         final StringWriter sw = new StringWriter(256);
-        CONTEXT.createMarshaller().marshal(ADAPTER.marshal(message), sw);
+        CONTEXT.createMarshaller().marshal(adapter().marshal(message), sw);
         return sw.toString();
     }
 
     static UpdateMessage decode(String string) throws Exception {
-        return (UpdateMessage) ADAPTER.unmarshal(
+        return (UpdateMessage) adapter().unmarshal(
                 (CompactUpdateMessageDto) CONTEXT.createUnmarshaller()
                     .unmarshal(new StringReader(string)));
+    }
+
+    private static UpdateMessageAdapter adapter() {
+        return new UpdateMessageAdapter();
     }
 
     private JAXB() { }
