cdc9dd72bb956fbbb3bd567cdf0950c410b5017b
Christian Schlichtherle <christian@schlichtherle.de>
2013-10-20 15:26:20 +0200

    Javadoc.

--- trueupdate-core/src/main/java/net/java/trueupdate/core/io/InputTask.java
+++ trueupdate-core/src/main/java/net/java/trueupdate/core/io/InputTask.java
@@ -4,12 +4,14 @@
  */
 package net.java.trueupdate.core.io;
 
-import java.io.*;
+import java.io.InputStream;
 import javax.annotation.WillNotClose;
 
 /**
  * Executes a task on an {@link InputStream}.
  *
+ * @param <V> the type of the result.
+ * @param <X> the type of the exception.
  * @see Sources#execute
  * @author Christian Schlichtherle
  */
--- trueupdate-core/src/main/java/net/java/trueupdate/core/io/OutputTask.java
+++ trueupdate-core/src/main/java/net/java/trueupdate/core/io/OutputTask.java
@@ -4,12 +4,14 @@
  */
 package net.java.trueupdate.core.io;
 
-import java.io.*;
+import java.io.OutputStream;
 import javax.annotation.WillNotClose;
 
 /**
  * Executes a task on an {@link OutputStream}.
  *
+ * @param <V> the type of the result.
+ * @param <X> the type of the exception.
  * @see Sinks#execute
  * @author Christian Schlichtherle
  */
