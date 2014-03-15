1d9534b8db53631efa78f8d0561c8ef15d17fa8a
Christian Schlichtherle <christian@schlichtherle.de>
2014-03-15 15:12:30 +0100

    Had to revert change for backward compatibility.

--- trueupdate-installer/trueupdate-installer-tomcat/src/main/java/net/java/trueupdate/installer/tomcat/TomcatUpdateInstaller.java
+++ trueupdate-installer/trueupdate-installer-tomcat/src/main/java/net/java/trueupdate/installer/tomcat/TomcatUpdateInstaller.java
@@ -74,14 +74,14 @@ public final class TomcatUpdateInstaller extends CoreUpdateInstaller {
 
             final File appBase = appBase();
 
-            final ContextName ccn = new ContextName(uc.currentLocation(), true);
+            final ContextName ccn = new ContextName(uc.currentLocation());
             final String cname = ccn.getName();
             final File cdir = new File(appBase, ccn.getBaseName());
             final File cwar = new File(cdir.getPath() + ".war");
 
             final boolean warDeployment = cwar.isFile();
 
-            final ContextName ucn = new ContextName(uc.updateLocation(), true);
+            final ContextName ucn = new ContextName(uc.updateLocation());
             final String uname = ucn.getName();
             final File udir = new File(appBase, ucn.getBaseName());
             final File uwar = new File(udir.getPath() + ".war");
