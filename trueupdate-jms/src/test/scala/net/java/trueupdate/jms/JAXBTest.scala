2387e69880b6b22c6e99edfc81af8d792369d383
Christian Schlichtherle <christian@schlichtherle.de>
2014-12-28 18:04:48 +0100

    Simplified test.

--- trueupdate-jms/src/test/scala/net/java/trueupdate/jms/JAXBTest.scala
+++ trueupdate-jms/src/test/scala/net/java/trueupdate/jms/JAXBTest.scala
@@ -5,14 +5,16 @@
 package net.java.trueupdate.jms
 
 import java.util.logging._
-import org.junit.runner.RunWith
-import org.scalatest.WordSpec
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.Matchers._
-import org.scalatest.prop.PropertyChecks._
+
 import net.java.trueupdate.artifact.spec.ArtifactDescriptor
 import net.java.trueupdate.message.UpdateMessage
-import net.java.trueupdate.message.UpdateMessage._
+import net.java.trueupdate.message.UpdateMessage.{builder, _}
+import org.junit.runner.RunWith
+import org.scalatest.Matchers._
+import org.scalatest.WordSpec
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.prop.PropertyChecks._
+import JAXBTest._
 
 /**
  * @author Christian Schlichtherle
@@ -20,9 +22,51 @@ import net.java.trueupdate.message.UpdateMessage._
 @RunWith(classOf[JUnitRunner])
 class JAXBTest extends WordSpec {
 
-  type Int = java.lang.Integer
+  "An update message" should {
+    "be round-trip XML-serializable" in {
+      val table = Table(
+        "originalMessage",
+        builder
+          .from("from")
+          .to("to")
+          .`type`(Type.UPDATE_NOTICE)
+          .artifactDescriptor
+            .groupId("groupId")
+            .artifactId("artifactId")
+            .version("version")
+            .inject
+          .currentLocation("currentLocation")
+          .build,
+        addLogRecord(builder
+          .from("from")
+          .to("to")
+          .`type`(Type.UPDATE_NOTICE)
+          .artifactDescriptor
+            .groupId("groupId")
+            .artifactId("artifactId")
+            .version("version")
+            .classifier("classifier")
+            .packaging("extension")
+            .inject
+          .updateVersion("updateVersion")
+          .currentLocation("currentLocation")
+          .updateLocation("updateLocation")
+          .build)
+      )
+      forAll(table) { originalMessage =>
+        val encoding = JAXB.encode(originalMessage)
+        logger log (Level.FINE, "\n{0}", encoding)
+        encoding should not include "xsi:type"
+        val clonedMessage = JAXB.decode(encoding)
+        clonedMessage should equal (originalMessage)
+      }
+    }
+  }
+}
 
-  def builder = UpdateMessage.builder
+object JAXBTest {
+
+  val logger = Logger.getLogger(getClass.getName)
 
   def addLogRecord(message: UpdateMessage) = {
     val ad = ArtifactDescriptor
@@ -41,59 +85,11 @@ class JAXBTest extends WordSpec {
     setSourceClassName(classOf[JAXBTest].getName)
     setSourceMethodName("logRecord")
     setMessage("test")
-    setParameters(Array(1: Int, 2: Int, 3: Int, ad))
+    setParameters(Array(1: Integer, 2: Integer, 3: Integer, ad))
     setThreadID(Int.MinValue)
     setMillis(System.currentTimeMillis)
     setThrown(new Throwable)
     message.attachedLogs.add(lr)
     message
   }
-
-  val subjects = Table(
-    "original",
-    builder
-      .from("from")
-      .to("to")
-      .`type`(Type.UPDATE_NOTICE)
-      .artifactDescriptor
-        .groupId("groupId")
-        .artifactId("artifactId")
-        .version("version")
-        .inject
-      .currentLocation("currentLocation")
-      .build,
-    addLogRecord(builder
-      .from("from")
-      .to("to")
-      .`type`(Type.UPDATE_NOTICE)
-      .artifactDescriptor
-        .groupId("groupId")
-        .artifactId("artifactId")
-        .version("version")
-        .classifier("classifier")
-        .packaging("extension")
-        .inject
-      .updateVersion("updateVersion")
-      .currentLocation("currentLocation")
-      .updateLocation("updateLocation")
-      .build)
-  )
-
-  "An update message" when {
-    "constructed" should {
-      "be round-trip XML-serializable" in {
-        forAll(subjects) { original =>
-          val originalEncoding = JAXB.encode(original)
-          JAXBTest.logger log (Level.FINE, "\n{0}", originalEncoding)
-          originalEncoding contains "xsi:type" should not be true
-          val clone = JAXB.decode(originalEncoding)
-          clone should equal (original)
-        }
-      }
-    }
-  }
-}
-
-object JAXBTest {
-  val logger = Logger.getLogger(getClass.getName)
 }
